273e76d1e3f6f3ba92cd3cde7cc0ccd753b65ab3
mkaif404 <mkaif404@gmail.com>
2025-04-18 16:03:47 +0530

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -10,7 +10,7 @@ function Card({ id, name, info, image, price , removeTour}) {
 
   return (
     <div className="card">
-      <img src={image} className="image"></img>
+      <img src={image} className="image" alt="image"></img>
 
       <div className="tour-info">
         <div className="tour-details">
